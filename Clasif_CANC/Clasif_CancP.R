8aa331579a285fd824e75dd7d32e6ad0467a3b7d
Daniel Loera <daniel87loera@gmail.com>
2018-08-06 10:34:47 -0500

    t

--- Clasif_CANC/Clasif_CancP.R
+++ Clasif_CANC/Clasif_CancP.R
@@ -1,52 +1,51 @@
 #############################################################
 ########### CONECTOR A BD    ################################
 #############################################################
-
 #library("RODBC")
 #odbcChannel <- odbcConnect("DataClasificacion", uid = "sa", pwd = "Tibs2016")
 #ABA_NivelPoliza <- sqlFetch(odbcChannel, "Info_Aba_NivelPoliza")
-
-
 #############################################################
 ###################### LECTOR DE ARCHIVO ####################
 #############################################################
+#CC <- function(){
 ABA_NivelPoliza <- ("C:\\Users\\V Tibs\\Desktop\\R\\R\\Clasif_CANC\\ABA_NivelPoliza.csv")
 ABA_NP <- read.csv(ABA_NivelPoliza,header = TRUE)
 #names(ABA_NivelPoliza)<- c("CvePoliza","CvePolizaPrevia","InVigPoliza", "FinVigPoliza","Producto","TipoEmision",
 #                           "TiendaDanos","SucursalDanos","OfVntDanos", "TipoProducto","PrimaNetaPesos","SumaAsegurada", "EstPoliza")
-
 ############################################################
 ############ ENTRENAMIENTO DE RED  Y TESTEO DE #############
 ##################TEOREMA DE BAYES #########################
 ############################################################
-
 library(e1071)
 # Selección de una submuestra de 105 (el 70% de los datos)
 set.seed(101)
 ABA.indices <- sample(1:nrow(ABA_NP), nrow(ABA_NP) * 0.7)
 ABA.entrenamiento <- ABA_NP[ABA.indices,]
 ABA.test <- ABA_NP[-ABA.indices,]
-
 model <- naiveBayes(EstPoliza ~ ., data = ABA.entrenamiento)
-
 # Importancia de cada variable
 #model$importance
-
 ##################################################################
 ####################### PREDICCION ###############################
 ##################################################################
-# predict necesita el parámetro newdata
+#redict necesita el parámetro newdata
 results <- predict(object = model, newdata = ABA.test, type = "raw")
-results2 <- predict(object = model, ABA.test, type = "class")
-
-mc <- table(results2, ABA.test$EstPoliza)
-df<- data.frame(mc)
+#results2 <- predict(object = model, ABA.test, type = "class")
+#mc <- table(results,ABA.test$EstPoliza)
+#mc
 Porcentaje <- round(results*100)
-
 # Correctamente clasificados
 x <- 100 * sum(diag(mc)) / sum(mc)
-
 #Agregar Columna de Prediccion a la tabla ABA.test
 ABA.test$PrediClas <- Porcentaje
-ABA.test$PClas <- results2
-#write.csv(ABA.test, "Clasif_Canc.csv")
+#ABA.test$PClas <- results2
+write.csv(ABA.test, "Clasif_Canc2.csv")
+################################################################
+########### PRUEBA DE ARCHIVO RECIBIDO #########################
+################################################################
+Prueba <- ("C:\\Users\\V Tibs\\Desktop\\Prueba.csv")
+PruebaR <- read.csv(Prueba, header = TRUE)
+PruebaP <- predict(object= model, newdata = PruebaR, type =  "raw")
+prue <- PruebaP * 100
+
+#}
